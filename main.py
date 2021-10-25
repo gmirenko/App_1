bff1dbee64ff1fa18aa1e03de999099b4e94ecde
gmirenko <gmirenkosergij@gmail.com>
2021-10-25 17:17:45 +0300

    menu add App_1

--- main.py
+++ main.py
@@ -38,12 +38,12 @@ KV = '''
             size: "56dp", "56dp"
             source: "data/logo/kivy-icon-256.png"
     MDLabel:
-        text: "KivyMD library"
+        text: "nick LeRA"
         font_style: "Button"
         size_hint_y: None
         height: self.texture_size[1]
     MDLabel:
-        text: "kivydevelopment@gmail.com"
+        text: "автор Гмиренко Валерия"
         font_style: "Caption"
         size_hint_y: None
         height: self.texture_size[1]
@@ -57,12 +57,18 @@ Screen:
                 BoxLayout:
                     orientation: 'vertical'
                     MDToolbar:
-                        title: "Приложение для Валерии"
+                        title: "Лучшее приложение созданное Лерой"
                         elevation: 10
-                        left_action_items: [['menu', lambda x: nav_drawer.set_state('open')]]
+                        left_action_items: [['menu', lambda x: nav_drawer.set_state("open")]]
+                        right_action_items: [["star-outline", lambda x: app.on_star_click()]]
+                        md_bg_color: 0, 0, 0, 1
+
                     MDTabs:
                         id: tabs
                         on_tab_switch: app.on_tab_switch(*args)
+                        height: "48dp"
+                        tab_indicator_anim: False
+                        background_color: 0.1, 0.1, 0.1, 1 
         MDNavigationDrawer:
             id: nav_drawer
             ContentNavigationDrawer:
@@ -94,30 +100,58 @@ class DrawerList(ThemableBehavior, MDList):
 
 
 class Valeria (MDApp):
+    title = "Valeria"
+    by_who = "автор Гмиренко Валерией"
+
     def build(self):
         return Builder.load_string(KV)
 
-    def on_start(self, fn_regular=None):
-        icons_item = {
-            "folder": "Мои файлы",
-            "account-multiple": "Поделиться со всеми",
-            "star": "Запустить",
-            "history": "Сбросить",
-            "checkbox-marked": "Поделиться со мной",
-            "upload": "Загрузить",
+    def on_start(self):
+        icons_item_menu_lines = {
+            "account-cowboy-hat": "об авторе",
+            "youtube": "My YouTube",
+            "coffee": "Donate автору",
+            "github": "source code",
+            "share-variant": "Share app",
+            "shield-sun": "Тема - Темная/Светлая",
         }
-        for icon_name in icons_item.keys():
+        icons_item_menu_tabs = {
+            "calculator-variant": "Input",
+            "table-large": "Table",
+            "chart-areaspline": "Graph",
+            "chart-pie": "source code",
+            "share-variant": "Chart",
+            "book-open-variant": "Sum",
+        }
+        for icon_name in icons_item_menu_lines.keys():
             self.root.ids.content_drawer.ids.md_list.add_widget(
-                ItemDrawer(icon=icon_name, text=icons_item[icon_name])
+                ItemDrawer(icon=icon_name, text=icons_item_menu_lines[icon_name])
             )
         # for name_tab in list(md_icons.keys())[15:30]:
         #     self.root.ids.tabs.add_widget(Tab(icon=name_tab, title=name_tab))
 
-        for icon_name, name_tab in icons_item.items():
+        for icon_name, name_tab in icons_item_menu_tabs.items():
             self.root.ids.tabs.add_widget(
-                Tab(text=f"[ref={name_tab}][font={fonts[-1]['fn_regular']}]{md_icons[icon_name]}[/font][/ref] {name_tab}"
-                )
-            )
+                Tab(
+                    text=f"[size=20][font={fonts[-1]['fn_regular']}]{md_icons[icon_name]}[size][/font] \
+                    {name_tab}"))
 
+
+
+    def on_tab_switch(self, instance_tabs, instance_tab, instance_tab_label, tab_text):
+        '''
+        Called when switching tabs.
+        :type instance_tabs: <kivymd.uix.tab.MDTabs object>;
+        :param instance_tab: <__main__.Tab object>;
+        :param instance_tab_label: <kivymd.uix.tab.MDTabsLabel object>;
+        :param tab_text: text or name icon of tab;
+        '''
+        # get the tab icon.
+        count_icon = instance_tab.icon
+        # print it on shell/bash.
+        print('tab clicked!'+ tab_text)
+
+    def on_star_click(self):
+        pass
 
 Valeria().run()
